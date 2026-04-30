eaf07ff9f28b6ec7c3773d9ec3074fe247da9073
David Fekke <david@fekke.com>
2026-04-30 18:50:55 -0400

    Adding Denz'ls scripts

--- models/model4_nlp_classification/train.py
+++ models/model4_nlp_classification/train.py
@@ -13,9 +13,12 @@ IMPORTANT: Save your vectorizer/tokenizer alongside the model — you'll need
 the same text preprocessing at prediction time.
 """
 from pathlib import Path
+import torch
 
-PROCESSED_DATA = Path("data/processed/")
-SAVED_MODEL_DIR = Path("models/model4_nlp_classification/saved_model/")
+ 
+
+PROCESSED_DATA = Path("data/raw/smart_city_csvs/urbanpulse_311_complaints.csv")
+SAVED_MODEL_DIR = Path("./models/model4_nlp_classification/saved_model")
 
 
 def load_data():
@@ -25,8 +28,10 @@ def load_data():
         from pipelines.data_pipeline import load_processed_data
         df = load_processed_data()
     """
-    # TODO: Load your text dataset
-    raise NotImplementedError
+    from pipelines.data_pipeline import load_raw_data
+    df = load_raw_data()
+    return df
+    
 
 
 def preprocess_text(texts):
@@ -34,7 +39,6 @@ def preprocess_text(texts):
 
     Common steps:
     - Lowercase
-    - Remove special characters, HTML tags
     - Handle abbreviations and slang
     - Tokenize
     - Remove stopwords (optional — sometimes they help)
@@ -42,7 +46,13 @@ def preprocess_text(texts):
     IMPORTANT: Apply the SAME preprocessing at prediction time.
     """
     # TODO: Clean your text data
-    raise NotImplementedError
+    from transformers import DistilBertTokenizerFast
+    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+    texts = list(texts)
+    tokenized_texts = tokenizer(texts, truncation = True, batched = True, max_length=128)
+    return tokenized_texts
+
+    
 
 
 def vectorize_text(texts):
@@ -61,7 +71,38 @@ def vectorize_text(texts):
         tokenizer.fit_on_texts(texts)
     """
     # TODO: Vectorize your text
-    raise NotImplementedError
+
+import torch
+class ComplaintDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {
+            key: torch.tensor(val[idx]) for key, val in self.encodings.items()
+        }
+        item["labels"] = torch.tensor(self.labels[idx], dtype=torch.long)
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+
+
+def compute_metrics(eval_pred):
+    import numpy as np
+    from sklearn.metrics import accuracy_score, f1_score
+    logits, labels = eval_pred
+    preds = np.argmax(logits, axis=1)
+
+    acc = accuracy_score(labels, preds)
+    f1 = f1_score(labels, preds, average="weighted")
+
+    return {
+        "accuracy": acc,
+        "weighted_f1": f1
+    }
 
 
 def train_model(X_train, y_train):
@@ -77,10 +118,49 @@ def train_model(X_train, y_train):
         model = tf.keras.Sequential([...])
     """
     # TODO: Train your model
-    raise NotImplementedError
+    from transformers import Trainer, TrainingArguments, DistilBertTokenizerFast, DistilBertForSequenceClassification, DataCollatorWithPadding
+    from sklearn.metrics import classification_report, f1_score, confusion_matrix
+    
+    train_dataset = ComplaintDataset(X_train, y_train)
+
+    model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=6)
+    data_collator = DataCollatorWithPadding(tokenizer=DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased'))
+
+    for param in model.distilbert.parameters():
+        param.requires_grad = False
+    for layer in model.distilbert.transformer.layer[-1:]:
+        param.requires_grad = True 
+    for param in model.pre_classifier.parameters():
+        param.requires_grad = True
+    for param in model.classifier.parameters():
+        param.requires_grad = True
+
+    training_args = TrainingArguments(
+        output_dir = SAVED_MODEL_DIR,
+        num_train_epochs = 3,
+        per_device_train_batch_size = 16,
+        learning_rate = 2e-5,
+        weight_decay = 0.01,
+        save_strategy = "epoch",
+        logging_strategy = "epoch",
+    )
+
+    trainer = Trainer(
+    model = model,
+    args = training_args,
+    train_dataset = train_dataset,
+    data_collator = data_collator,
+    compute_metrics = compute_metrics,
+    )
+    
+    trainer.train()
+
+    return trainer
+
+    
 
 
-def evaluate_model(model, X_val, y_val):
+def evaluate_model(trainer, X_val, y_val):      #model, X_val, y_val):
     """Evaluate NLP model performance.
 
     Must include:
@@ -90,10 +170,30 @@ def evaluate_model(model, X_val, y_val):
     - Example predictions with actual text
     """
     # TODO: Evaluate your model
-    raise NotImplementedError
+    from sklearn.metrics import classification_report, f1_score, confusion_matrix
+    import numpy as np
+
+    val_dataset = ComplaintDataset(X_val, y_val)
+
+    predictions = trainer.predict(val_dataset)
+
+    logits = predictions.predictions
+    y_true = predictions.label_ids
+    y_pred = np.argmax(logits, axis=1)
+
+    print("Weighted F1:")
+    print(f1_score(y_true, y_pred, average="weighted"))
+
+    print("\nClassification Report:")
+    print(classification_report(y_true, y_pred))
+
+    print("\nConfusion Matrix:")
+    print(confusion_matrix(y_true, y_pred))
+
+    
 
 
-def save_model(model):
+def save_model(trainer):
     """Save model AND vectorizer/tokenizer.
 
     IMPORTANT: You must save both the model and the text preprocessor.
@@ -105,12 +205,58 @@ def save_model(model):
         joblib.dump(vectorizer, SAVED_MODEL_DIR / "vectorizer.joblib")
     """
     # TODO: Save your model and vectorizer
-    raise NotImplementedError
+    trainer.save_model(SAVED_MODEL_DIR)
+    trainer.tokenizer.save_pretrained(SAVED_MODEL_DIR)
+    
+import pandas as pd
+def create_complaint_categories(df: pd.DataFrame) -> pd.DataFrame:
+
+  
+    top_5 = ['Illegal Parking', 'HEAT/HOT WATER', 'Noise - Residential',
+             'Snow or Ice', 'Blocked Driveway']
+    df['complaint_category'] = df['complaint_type'].apply(
+        lambda x: x if x in top_5 else 'Other'
+        )
+
+    print("Complaint category distribution:")
+    print(df['complaint_category'].value_counts())
+
+    coverage = df[df['complaint_category'] != 'Other'].shape[0] / len(df) * 100
+    print(f"\nTop 5 categories cover {coverage:.1f}% of all complaints")
+    print(f"Total classes: {df['complaint_category'].nunique()} (top 5 + Other)")
+
+    return df
 
 
 def main():
     # 1. Load data
-    df = load_data()
+    import pandas as pd
+    from pathlib import Path
+    
+    df = pd.read_csv(Path("data/raw/smart_city_csvs/urbanpulse_311_complaints.csv"))
+
+    df = create_complaint_categories(df)
+
+    y = df['complaint_category']
+    X = df['resolution_description']
+
+    from sklearn.model_selection import train_test_split
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3,
+    random_state=42, stratify=y)
+
+    X_train_encoded = preprocess_text(X_train)
+    X_val_encoded = preprocess_text(X_val)
+
+    from sklearn.preprocessing import LabelEncoder
+    le = LabelEncoder()
+    y_train_encoded = le.fit_transform(y_train)
+    y_val_encoded = le.transform(y_val)
+
+    trainer = train_model(X_train_encoded, y_train_encoded)
+
+    evaluate_model(trainer, X_val_encoded, y_val_encoded)
+
+    save_model(trainer)
 
     # 2. Preprocess text
     # texts = preprocess_text(df["text_column"])
